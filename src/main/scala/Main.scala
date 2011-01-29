87fc6710f9a8fa052137e03e41bbe2ec97e70a3d
Alexander Grünewald <alexander.gruenewald@gmail.com>
2011-01-29 16:57:17 +0100

    added License header

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2011 by Alexander Grünewald
+ * 
+ * This file is part of gruenewa-dsig, a XML signature utility library.
+ *
+ * gruenewa-grid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 object Main {
   def main(args: Array[String]) {
 
@@ -11,8 +30,6 @@ object Main {
                 <bbb>Hello World!</bbb>
               </aaa>
 
-
-    // Load the KeyStore and get the signing key and certificate.
     val ks = KeyStore.getInstance("JKS");
     ks.load(new java.io.FileInputStream("mykeystore.jks"), "changeit".toCharArray());
     val keyEntry = ks.getEntry("mykey", new KeyStore.PasswordProtection("changeit".toCharArray())).asInstanceOf[KeyStore.PrivateKeyEntry];
