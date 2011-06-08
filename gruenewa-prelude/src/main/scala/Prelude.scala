1feb64b38841578003c81a1d9921b65070c78000
Alexander Gruenewald <alexander.gruenewald@gmail.com>
2011-06-08 20:06:34 +0200

    Adding new function timedRun() that allows to check for a timeout

--- gruenewa-prelude/src/main/scala/Prelude.scala
+++ gruenewa-prelude/src/main/scala/Prelude.scala
@@ -66,4 +66,22 @@ package object prelude {
     result
   }
 
+  /**
+   * Runs the given code block or throws
+   * an exception on timeout.
+   */ 
+  @throws(classOf[java.util.concurrent.TimeoutException])
+  def timedRun[F](timeout: Long)(f: => F): F = {
+
+    import java.util.concurrent.{Callable, FutureTask, TimeUnit}
+    
+    val task = new FutureTask(new Callable[F]() {
+      def call() = f
+    })
+
+    new Thread(task).start()
+    
+    task.get(timeout, TimeUnit.MILLISECONDS)  
+  }
+
 }
